35ac6bb8e3dbf23f6ec8728dc5936f828ea6b201
Rachel Lewis <rachel.e.lewis@proton.me>
2025-09-18 20:18:38 -0700

    Refine CORS middleware settings to restrict allowed methods + allow safelisted headers as default value

--- app/main.py
+++ app/main.py
@@ -14,8 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["OPTIONS", "GET", "POST"]
 )
 
 # ---------- Models ----------
