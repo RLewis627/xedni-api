48e68e2fdbd043a3d1e81a9a6427901c89d7d844
Rachel Lewis <rachel.e.lewis@proton.me>
2025-10-29 20:34:37 -0700

    Implemented a v2 /generate endpoint that uses Box-Muller transformation

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import datetime as dt
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 
 app = FastAPI()
 
@@ -51,6 +52,44 @@ def generate(req: GenerateRequest):
         shock = random.normalvariate((mu - 0.5 * sigma**2) * ds,
                                       sigma * math.sqrt(ds))
         prices.append(prices[-1] * math.exp(shock))
+
+    ## Build a fake OHLC dataframe
+    dates = [dt.datetime.today() - dt.timedelta(days=i) for i in range(req.days)]
+    dates = list(reversed(dates))
+
+    df = []
+    for i in range(len(dates)):
+        high = prices[i] * (1 + random.uniform(0, 0.02))
+        low = prices[i] * (1 - random.uniform(0, 0.02))
+        df.append({
+            "date": dates[i],
+            "open": prices[i],
+            "high": high,
+            "low": low,
+            "close": random.uniform(low, high),
+            "volume": random.randint(1000, 5000)
+        })
+
+    return df
+
+# Generate fake stock data using Box-Muller transformation
+@app.post("/v2/generate")
+def generate(req: GenerateRequest):
+    
+    rachels_age = (datetime.now() - datetime(1997, 12, 6)).seconds
+    random.seed(rachels_age)  # reproducible for testing
+
+    ds = 1 / 252  # daily steps
+    mu = 0.001  # mean
+    theta = 0.02   # standard deviation
+
+    prices = [req.start_price]
+    for _ in range(req.days - 1):
+        u1 = 1.0 - random.random()
+        u2 = 1.0 - random.random()
+        z0 = math.sqrt(-2.0 * math.log(u1)) * math.cos(2.0 * math.pi * u2)
+        z1 = mu + theta * z0
+        prices.append(prices[-1] * math.exp(z1))
 
     ## Build a fake OHLC dataframe
     dates = [dt.datetime.today() - dt.timedelta(days=i) for i in range(req.days)]
